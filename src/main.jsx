81b717813949c60a655e812795307f794a48c2fa
jaewoongkr <hjw12199@naver.com>
2024-08-15 16:31:06 +0900

    리덕스 추가 123

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './Router';
-import store from './store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
