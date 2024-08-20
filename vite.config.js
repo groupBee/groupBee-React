ed798f9b07fa6361d4ae891481e9317ad297eaa1
jaewoongkr <hjw12199@naver.com>
2024-08-20 11:01:24 +0900

    컨피그 수정

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy : {
-      '/api' : {
-        target : 'http://localhost:9522/',
-        changeOrigin : true
+export default defineConfig(({mode}) => {
+  // mode에 따라 .env 파일에서 환경 변수를 로드합니다.
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_IP,
+          changeOrigin: true
+        }
       }
     }
   }
