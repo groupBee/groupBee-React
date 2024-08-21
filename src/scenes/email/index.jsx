3828bca940aec836bf53a2d4199f0f64da214cfc
songawon <ok466sgw@naver.com>
2024-08-21 14:33:07 +0900

    email smtp구현

--- src/scenes/email/index.jsx
+++ src/scenes/email/index.jsx
@@ -1,10 +1,23 @@
+import { Button } from "react-bootstrap";
 import SendMail from "./sendEmail";
+import { useState } from "react";
 
 const EmailMain=()=>{
+    
     return(
         <div>
             <h1>Email</h1>
-            <SendMail/>
+            <div>
+                <Button variant="contained" color="info" >메일 보내기</Button>
+                <Button variant="contained" color="warning" >메일 확인하기</Button>
+            </div>
+            <div>
+                <SendMail/>
+            </div>
+            <div>
+
+            </div>
+            
         </div>
     )
 }
