aba2782e5d443b00e4f74a9e7efe4b8d07615101
songawon <ok466sgw@naver.com>
2024-08-21 17:41:09 +0900

    elecapp email

--- src/scenes/email/index.jsx
+++ src/scenes/email/index.jsx
@@ -1,24 +1,28 @@
 import { Button } from "react-bootstrap";
 import SendMail from "./sendEmail";
 import { useState } from "react";
+import EmailList from "./emailList";
 
-const EmailMain=()=>{
-    
-    return(
+const EmailMain = () => {
+    const [view, setView] = useState("none");
+
+    return (
         <div>
             <h1>Email</h1>
             <div>
-                <Button variant="contained" color="info" >메일 보내기</Button>
-                <Button variant="contained" color="warning" >메일 확인하기</Button>
+                <Button variant="contained" color="info" onClick={() => setView("send")}>
+                    메일 보내기
+                </Button>
+                <Button variant="contained" color="warning" onClick={() => setView("list")}>
+                    메일 확인하기
+                </Button>
             </div>
             <div>
-                <SendMail/>
+                {view === "send" && <SendMail />}
+                {view === "list" && <EmailList />}
             </div>
-            <div>
-
-            </div>
-            
         </div>
-    )
-}
+    );
+};
+
 export default EmailMain;
