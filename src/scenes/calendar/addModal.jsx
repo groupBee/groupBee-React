e7ee14f7877346670fbce78e7a4d85eca8fa1dbf
Jinwook Jung <jeenuk.chung@gmail.com>
2024-08-25 13:43:17 +0900

    차량 예약, 캘린더 일정 별 다른 modal 창 으로 변경

---
+++ src/scenes/calendar/addModal.jsx
@@ -0,0 +1,175 @@
+import React, {useEffect} from 'react';
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Button,
+    TextField,
+} from "@mui/material";
+
+const addModal = ({isOpen, onCancel, onSubmit, inputValues, setInputValues}) => {
+    useEffect(() => {
+        //console.log("Current Input Values:", inputValues);
+    }, [inputValues]);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setInputValues((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    return (
+        <Dialog open={isOpen} onClose={onCancel}>
+            <DialogTitle variant="h3" component="h2"
+                         sx={{
+                             fontSize: '1.5rem',
+                             marginBottom: '1px',
+                             backgroundImage: 'linear-gradient(to right, #FFA800, #FFD600)',
+                             color: 'white'
+                         }}>
+                일정 추가
+            </DialogTitle>
+            <DialogContent>
+                {inputValues && (
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                        label="제목"
+                        name="title"
+                        value={inputValues.title || ''}
+                        onChange={handleChange}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '&:hover fieldset': {
+                                    borderColor: '#ffb121',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#ffb121',
+                                },
+                            },
+                            '&:hover': {
+                                '& .MuiInputLabel-root': {
+                                    color: '#ffb121',
+                                },
+                            },
+                        }}
+                    />)}
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    margin="normal"
+                    label="시작 시간"
+                    name="startDay"
+                    type="datetime-local"
+                    value={inputValues.startDay || ''}
+                    onChange={handleChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    sx={{
+                        '& .MuiOutlinedInput-root': {
+                            '&:hover fieldset': {
+                                borderColor: '#ffb121',
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: '#ffb121',
+                            },
+                        },
+                        '&:hover': {
+                            '& .MuiInputLabel-root': {
+                                color: '#ffb121',
+                            },
+                        },
+                    }}
+                />
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    margin="normal"
+                    label="끝나는 시간"
+                    name="endDay"
+                    type="datetime-local"
+                    value={inputValues.endDay || ''}
+                    onChange={handleChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    sx={{
+                        '& .MuiOutlinedInput-root': {
+                            '&:hover fieldset': {
+                                borderColor: '#ffb121',
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: '#ffb121',
+                            },
+                        },
+                        '&:hover': {
+                            '& .MuiInputLabel-root': {
+                                color: '#ffb121',
+                            },
+                        },
+                    }}
+                />
+                {inputValues && (
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                        label="내용"
+                        name="content"
+                        value={inputValues.content || ''}
+                        onChange={handleChange}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '&:hover fieldset': {
+                                    borderColor: '#ffb121',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#ffb121',
+                                },
+                            },
+                            '&:hover': {
+                                '& .MuiInputLabel-root': {
+                                    color: '#ffb121',
+                                },
+                            },
+                        }}
+                    />)}
+
+                <DialogActions mt={2}>
+                    <Button onClick={onSubmit} variant="outlined" color="secondary"
+                            sx={{
+                                fontSize: '1rem',
+                                color: '#ffb121',
+                                backgroundColor: 'white',
+                                border: '1px solid #ffb121',
+                                '&:hover': {
+                                    backgroundColor: 'white',
+                                    color: '#ffb121',
+                                    border: '1px solid #ffb121',
+                                },
+                            }}>추가</Button>
+
+                    <Button onClick={onCancel} variant="outlined" color="secondary"
+                            sx={{
+                                fontSize: '1rem',
+                                color: '#ffb121',
+                                backgroundColor: 'white',
+                                border: '1px solid #ffb121',
+                                '&:hover': {
+                                    backgroundColor: 'white',
+                                    color: '#ffb121',
+                                    border: '1px solid #ffb121',
+                                },
+                            }}>취소</Button>
+                </DialogActions>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default addModal;
---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,283 @@
+import {Box, List, ListItem, ListItemText, Tooltip, Typography, useMediaQuery, useTheme,} from "@mui/material";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import {tokens} from "../../theme";
+import {useEffect, useRef, useState} from "react";
+import {Header} from "../../components";
+import {formatDate} from "@fullcalendar/core";
+import useModal from "./useModal";
+import './calendar.css';
+
+const Calendar = () => {
+    const [events, setEvents] = useState([]);
+    const [tooltipContent, setTooltipContent] = useState('');
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+
+    /* 백엔드 데이터 리스트 출력 */
+    const fetchData = async () => {
+        try {
+            const calendarResponse = await fetch("/api/calendar/list");
+            const calendarData = await calendarResponse.json();
+            const events = calendarData.map((event, index) => ({
+                id: event.id,
+                title: event.title,
+                content: event.content,
+                start: event.startDay,
+                end: event.endDay,
+                backgroundColor: '#ffc107',     // 이벤트 배경색
+                borderColor: '#ffc107',         // 이벤트 테두리색
+                textColor: '#111111',
+            }));
+            setEvents(events);
+            console.log(events);
+        } catch (e) {
+            console.error('Error fetching data:', e);
+        }
+    };
+
+    /* 대한민국 공휴일 받아오기 위한 이벤트 */
+    const [googleEvents, setGoogleEvents] = useState([]);
+    const CALENDAR_ID = 'ko.south_korea#holiday@group.v.calendar.google.com';
+    const API_KEY = import.meta.env.VITE_GOOGLE_CALENDAR_API_KEY;
+
+    useEffect(() => {
+        fetchData(); // 백엔드 이벤트 가져오기
+    }, []);
+
+    /* fullcalendar 관련된 설정 */
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const isMdDevices = useMediaQuery("(max-width:920px)");
+    const isSmDevices = useMediaQuery("(max-width:600px)");
+    const isXsDevices = useMediaQuery("(max-width:380px)");
+    const [currentEvents, setCurrentEvents] = useState([]);
+
+    /* 특정 날짜에 해당하는 이벤트 필터링 */
+    const filterEventsForDate = (events, date) => {
+        return events.filter(event => {
+            const eventDate = new Date(event.start).toISOString().split('T')[0];
+            return eventDate === date.toISOString().split('T')[0];
+        });
+    };
+
+    /* Custom Modal 사용을 위함 */
+    const {showModal, deleteModal, modal} = useModal();
+
+    /* 캘린더 데이터 추가를 위한 이벤트 */
+    const handleDateClick = async (selected) => {
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+
+        // 선택된 날짜를 ISO 8601 형식으로 변환
+        const startStr = selected.startStr.slice(0, 10) + 'T00:30';
+        let endStr = selected.endStr.slice(0, 10) + 'T00:30';
+
+        if (selected.endStr) {
+            let endDate = new Date(selected.endStr);
+            endDate.setDate(endDate.getDate() - 1);
+            endStr = endDate.toISOString().slice(0, 10) + 'T00:30';
+        }
+
+        const inputValues = await showModal("일정 추가", "일정을 입력하세요:", 'prompt', {
+            startDay: startStr,
+            endDay: endStr
+        });
+
+        if (inputValues) {
+            const {title, startDay, endDay, content} = inputValues;
+
+            const event = {
+                id: `${selected.startStr}-${title}`,
+                title,
+                start: new Date(startDay),
+                end: new Date(endDay),
+                content,
+                backgroundColor: '#ffc107',
+                borderColor: '#ffc107'
+            };
+
+            calendarApi.addEvent(event);
+            fetchData();
+        }
+    };
+
+    /* 캘린더 데이터 삭제를 위한 이벤트 */
+    const handleEventClick = async (selected) => {
+        try {
+            const eventFromDb = await fetch(`/api/calendar/${selected.event.id}`).then(response => response.json());
+
+            const eventData = {
+                id: eventFromDb.id,
+                title: eventFromDb.title,
+                content: eventFromDb.content,
+                startDay: eventFromDb.startDay,
+                endDay: eventFromDb.endDay,
+                bookType: eventFromDb.bookType,
+                corporateCarId: eventFromDb.corporateCarId,
+            };
+
+            let result;
+            if (eventData.bookType === 0) {
+                result = await showModal(eventData.id, eventData.title, 'confirm', eventData);
+            } else {
+                result = await deleteModal(eventData.id, eventData.title, 'delete', eventData);
+            }
+
+            if (result) fetchData();
+        } catch (error) {
+            console.error('Calendar index handleEventClick error:', error);
+        }
+    };
+
+    return (
+        <Box m="20px">
+            <Header title="Calendar" subtitle="Full Calendar Interactive Page"/>
+            <Box display="flex" justifyContent="space-between" gap={2}>
+                {/* CALENDAR SIDEBAR */}
+                <Box
+                    display={`${isMdDevices ? "none" : "block"}`}
+                    flex="1 1 20%"
+                    bgcolor={colors.primary[400]}
+                    p="15px"
+                    borderRadius="4px"
+                >
+                    <Typography variant="h5">오늘의 일정</Typography>
+                    <List>
+                        {filteredEvents.map((event) => (
+                            <ListItem
+                                key={event.id}
+                                sx={{
+                                    bgcolor: `#ffb121`,
+                                    my: "10px",
+                                    borderRadius: "5px",
+                                }}
+                            >
+                                <ListItemText
+                                    primary={event.title}
+                                    primaryTypographyProps={{
+                                        sx: {
+                                            fontSize: "1.1em",
+                                            fontWeight: "bold"
+                                        }
+                                    }}
+                                    secondary={
+                                        <Typography>
+                                            {`${new Date(event.start).toLocaleString('ko-KR', {
+                                                month: 'long',
+                                                day: 'numeric',
+                                                hour: '2-digit',
+                                                hour12: false
+                                            })}`}
+                                            {` ~ ${new Date(event.end).toLocaleString('ko-KR', {
+                                                month: 'long',
+                                                day: 'numeric',
+                                                hour: '2-digit',
+                                                hour12: false
+                                            })}`}
+                                        </Typography>
+                                    }
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+
+                </Box>
+
+                {/* CALENDAR */}
+                <Box flex="1 1 100%">
+                    <FullCalendar
+                        height="75vh"
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin, googleCalendarPlugin]}
+                        headerToolbar={{
+                            left: `${isSmDevices ? "prev,next" : "prev,next today"}`,
+                            center: "title",
+                            right: `${
+                                isXsDevices
+                                    ? ""
+                                    : isSmDevices
+                                        ? "dayGridMonth,listMonth"
+                                        : "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                            }`,
+                        }}
+                        initialView="dayGridMonth"
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        googleCalendarApiKey={API_KEY}
+                        dayCellContent={(info) => info.date.getDate()}
+                        showNonCurrentDates={false}
+                        select={handleDateClick}
+                        eventClick={handleEventClick}
+                        events={events}
+                        eventsSet={(events) => {
+                            setCurrentEvents(events);
+                            const today = new Date();
+                            const filtered = filterEventsForDate(events, today);
+                            setFilteredEvents(filtered);
+                        }}
+                        eventMouseEnter={(info) => {
+                            setTooltipContent(info.event.extendedProps.content);
+                            setAnchorEl(info.el);
+                            setTooltipOpen(true);
+                        }}
+                        eventMouseLeave={() => {
+                            setTooltipOpen(false);
+                            setAnchorEl(null);
+                        }}
+                        eventTimeFormat={{
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            meridiem: false,
+                        }}
+                        locale='ko'
+                    />
+                    <Tooltip
+                        open={tooltipOpen}
+                        title={tooltipContent}
+                        placement="bottom"
+                        PopperProps={{
+                            anchorEl: anchorEl,
+                            disablePortal: true,
+                            sx: {
+                                '.MuiTooltip-tooltip': {
+                                    fontSize: '13px',
+                                    color: '#ffffff',
+                                    backgroundColor: '#333333',
+                                    borderRadius: '4px',
+                                    padding: '8px',
+                                }
+                            },
+                            modifiers: [
+                                {
+                                    name: 'preventOverflow',
+                                    options: {
+                                        padding: 8,
+                                    },
+                                },
+                                {
+                                    name: 'flip',
+                                    options: {
+                                        fallbackPlacements: ['top', 'bottom'],
+                                    },
+                                },
+                            ],
+                        }}
+                        style={{position: 'fixed'}}
+                    >
+                        <span/>
+                    </Tooltip>
+                </Box>
+            </Box>
+            {modal}
+        </Box>
+    );
+};
+
+export default Calendar;
