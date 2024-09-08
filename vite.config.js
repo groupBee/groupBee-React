e8431453fdd6d4153edfd314e254aeeba7ff56b4
songawon <ok466sgw@naver.com>
2024-09-08 18:44:33 +0900

    chatting

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
-import {defineConfig, loadEnv} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   // mode에 따라 .env 파일에서 환경 변수를 로드합니다.
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
+    define: {
+      global: {},  // 브라우저 환경에서 global을 빈 객체로 정의
+    },
     server: {
       host: '0.0.0.0',
       port: 5173,
       proxy: {
         '/api': {
           target: env.VITE_SERVER_IP,
-          changeOrigin: true
-        }
-      }
-    }
-  }
-})
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
